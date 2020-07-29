8362f4930688218e718e9bd613370c6d40f3c955
Søren Hougaard Mulvad <shmulvad@gmail.com>
2020-07-30 07:20:31 +0800

    Add custom colors to social icons

--- components/general/social-icons.js
+++ components/general/social-icons.js
@@ -1,17 +1,37 @@
+import { useState } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { socialIcons } from "../../data/contact";
 
-const SocialIcons = () => (
-  <ul className="icons">
-    {socialIcons.map((s) => (
-      <li key={s.label}>
-        <a href={s.link}>
-          <FontAwesomeIcon icon={s.icon} height="1em" alt={s.label} />
-        </a>
-      </li>
-    ))}
-  </ul>
-);
+const setColorsStatic = (curLabel) => {
+  return socialIcons.reduce((acc, icon) => {
+    const color = icon.label === curLabel ? icon.color : null;
+    acc[icon.label] = color;
+    return acc;
+  }, {});
+};
+
+const SocialIcons = () => {
+  const [colors, setColors] = useState(setColorsStatic(null));
+  return (
+    <ul className="icons">
+      {socialIcons.map((s) => (
+        <li key={s.label}>
+          <a href={s.link}>
+            <FontAwesomeIcon
+              icon={s.icon}
+              alt={s.label}
+              color={colors[s.label]}
+              height="1em"
+              onMouseOver={() => setColors(setColorsStatic(s.label))}
+              onMouseLeave={() => setColors(setColorsStatic(null))}
+            />
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default SocialIcons;
