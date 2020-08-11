60f2caf7375daa258362c35cac5d2c378031dfed
Søren Hougaard Mulvad <shmulvad@gmail.com>
2020-08-12 00:33:22 +0800

    Remove unnecessary async from function in feed

--- pages/api/feed.json.js
+++ pages/api/feed.json.js
@@ -2,7 +2,7 @@ import generateFeed from "../../lib/rss-feed";
 
 export default async (req, res) => {
   try {
-    const feed = await generateFeed("json");
+    const feed = generateFeed("json");
     const rssjson = feed.json1();
     res.setHeader("Content-type", "text/json");
     res.status(200).json(rssjson);
--- pages/api/feed.xml.js
+++ pages/api/feed.xml.js
@@ -2,7 +2,7 @@ import generateFeed from "../../lib/rss-feed";
 
 export default async (req, res) => {
   try {
-    const feed = await generateFeed("xml");
+    const feed = generateFeed("xml");
     const rssXml = feed.rss2();
     res.setHeader("Content-type", "text/xml; charset=utf-8");
     res.status(200).send(rssXml);
